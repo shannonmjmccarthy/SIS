267b6ec6f43503681203f6154fb090fa7211bcb4
shann <shann@DESKTOP-2478OFU.lan>
2024-03-07 11:39:30 -0700

    here is my first commit from home

--- SIS/src/Shannon.java
+++ SIS/src/Shannon.java
@@ -6,7 +6,7 @@ public class Shannon {
 		System.out.println("hello teammates");
 		//here is my submission from acorn2
 		System.out.println("help");
-		
+		String help = "i hope this is not on master branch";
 
 	}
 
