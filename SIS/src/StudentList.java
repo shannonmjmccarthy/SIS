830e7e7fb07dc4e806b45bee133363f1708a669d
michael.gifford <michael.gifford@MU-C1-09.mullen.pvt.k12.co.us>
2024-03-07 13:23:58 -0700

    student list

--- SIS/src/StudentList.java
+++ SIS/src/StudentList.java
@@ -1,4 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.IOException;
 
 public class StudentList {
-
+	ArrayList<Student> studentList = new ArrayList<>();
+	readFile();
+    public static void readFile() {
+    	
+	}
+    
+	
 }
