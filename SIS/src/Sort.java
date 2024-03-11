45df3b75e5f487b499206710e13b8e1f7bb4de54
allison.kozlowski <allison.kozlowski@MU-C1-03.mullen.pvt.k12.co.us>
2024-03-11 10:34:36 -0600

    start of sort class commit

---
+++ SIS/src/Sort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Sort {
+
+
+	
+	public static void run()
+	{
+		System.out.println("Sort Students:");
+		System.out.println("1) sort by last name");
+		System.out.println("2) sort by GPA");
+		System.out.println("3) sort by period");
+		Scanner userInput = new Scanner(System.in);
+		int option = userInput.nextInt();
+		
+		if(option == 1)
+		{
+			lastName();
+		}
+		else if(option == 2)
+		{
+			GPA();
+		}
+		else if(option == 3)
+		{
+			period();
+		}
+	}
+	
+	public static void lastName()
+	{
+		System.out.println("last name");
+	}
+	
+	public static void GPA()
+	{
+		System.out.println("GPA");
+	}
+	
+	public static void period()
+	{
+		System.out.println("period");
+	}
+
+}
