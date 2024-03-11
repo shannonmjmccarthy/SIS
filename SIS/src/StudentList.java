d4a0ae177dbe9ffef97aa7c7b1c07834e79d2b75
michael.gifford <michael.gifford@MU-C1-09.mullen.pvt.k12.co.us>
2024-03-11 10:38:05 -0600

    student list and array

--- SIS/src/StudentList.java
+++ SIS/src/StudentList.java
@@ -7,7 +7,21 @@ public class StudentList {
 	ArrayList<Student> studentList = new ArrayList<>();
 	readFile();
     public static void readFile() {
-    	
+    	Scanner studentFile = new Scanner(new File(“studentlist.txt”));
+    	while(studentFile.hasNext())
+    	{
+    	String firstName = studentFile.next();
+    	String lastName = studentFile.next();
+    	double gpa = 0;
+    	String firstClass = studentFile.next();
+    	String firstGrade = studentFile.next();
+    	String secondClass = studentFile.next();
+    	String secondGrade = studentFile.next();
+    	String thirdClass = studentFile.next();
+    	String thirdGrade = studentFile.next();
+    	studentList.add(new Student ) 
+    	}
+
 	}
     
 	
