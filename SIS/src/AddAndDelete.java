fe75214cd7b22d84613bfa5ae24789d321dd77db
shannon.mccarthy <shannon.mccarthy@MU-C1-07.mullen.pvt.k12.co.us>
2024-03-18 10:35:38 -0600

    added to add and delete

--- SIS/src/AddAndDelete.java
+++ SIS/src/AddAndDelete.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
+
 
 public class AddAndDelete {
-
+	static Scanner userIntInput = new Scanner(System.in);
+	static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Do you want to (1) add, or (2) delete a stdent?");
+		int answer = scanner.nextInt();
+		if(answer == 1) {
+			
+		}else if(answer == 2) {
+			
+		}
+		
 
 	}
 
+
+
+public static void add(){
+	System.out.println("Which student would you like to add?");
+	String first;
+	String last;
+	double gpa;
+	String grade1;
+	String grade2;
+	String grade3;
+	String class1;
+	String class2;
+	String class3;
+	
+	//StudentList.studentList.add(first, las);
+}
 }
